2306ab5f07db48f42d625ce69d96f930570b6ae1
Mariano Fernandez <marianofndz2480@gmail.com>
2024-06-04 19:09:59 -0300

    feat: delete unnecessary application classes

--- src/application/access-manager.ts
+++
@@ -1,7 +0,0 @@
-import { Session } from '../domain'
-
-export class AccessManager {
-    isAuthorized(_: Session): Promise<boolean> {
-        return Promise.resolve(true)
-    }
-}
--- src/application/database-connector.ts
+++
@@ -1,3 +0,0 @@
-export abstract class DatabaseConnector {
-    abstract connect(): Promise<void>
-}
