01ced28517869fd4c7f1f0d0bbe1b278ec472a97
Mariano Fernández <marianofndz2480@gmail.com>
2024-09-15 11:57:26 -0300

    feat: add middleware dependencies as object

--- src/application/handler/middleware.ts
+++ src/application/handler/middleware.ts
@@ -5,5 +5,9 @@ import { UseCase } from '../usecase'
 
 export abstract class Middleware<Dependencies extends Record<string, any> = {}> extends BaseClass<Dependencies> {
     declare public: boolean
-    abstract process(rawRequest: any, request: CommonRequest, useCase: UseCase<Session, CommonRequest>): Promise<void>
+    abstract process(data: {
+        rawRequest: any
+        request: CommonRequest
+        useCase: UseCase<Session, CommonRequest>
+    }): Promise<void>
 }
